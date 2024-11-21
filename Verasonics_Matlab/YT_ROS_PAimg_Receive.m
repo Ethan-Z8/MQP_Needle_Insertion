9c8f8d59faf3899e4abe236e7eb3cb604b691b12
Ethan Z <ezhong888@gmail.com>
2024-11-21 16:15:39 -0500

    different data display methods and makeing sue we are getting the right images

--- Verasonics_Matlab/YT_ROS_PAimg_Receive.m
+++ Verasonics_Matlab/YT_ROS_PAimg_Receive.m
@@ -14,11 +14,19 @@ PAIMG_msg = receive(PAIMG_sub);
 PAIMG = PAIMG_msg.Data;
 
 PAIMG0 = reshape(PAIMG,570,500);
+matrix_sum = sum(PAIMG0);
+
+imshow(PAIMG0)
+image(PAIMG0)
 imagesc(PAIMG0)
+
+
 colormap gray
 drawnow
 t = toc;
 fprintf(['Frame #',num2str(k),'  F=',num2str(1/(t-t1)),'Hz\n'])
 k = k+1;
 t1 = t;
+
+
 end

0b253cb72993669e33da49a96d472a7e6a18c905
Ethan Z <ezhong888@gmail.com>
2024-11-22 10:39:44 -0500

    figure out how to impliment CV in matlab

--- Verasonics_Matlab/YT_ROS_PAimg_Receive.m
+++ Verasonics_Matlab/YT_ROS_PAimg_Receive.m
@@ -2,31 +2,3 @@ clear all
 close all
 clc
 
-rosshutdown;
-
-rosinit
-  
-PAIMG_sub = rossubscriber('PA_IMG', 'std_msgs/Float64MultiArray');
-k = 1; t1=0;
-tic;
-while 1
-PAIMG_msg = receive(PAIMG_sub);
-PAIMG = PAIMG_msg.Data;
-
-PAIMG0 = reshape(PAIMG,570,500);
-matrix_sum = sum(PAIMG0);
-
-imshow(PAIMG0)
-image(PAIMG0)
-imagesc(PAIMG0)
-
-
-colormap gray
-drawnow
-t = toc;
-fprintf(['Frame #',num2str(k),'  F=',num2str(1/(t-t1)),'Hz\n'])
-k = k+1;
-t1 = t;
-
-
-end

ae9759d7da448488b9fcb78dfbe2e74d9280c852
Ethan Z <ezhong888@gmail.com>
2024-12-06 04:21:57 -0500

    starting to convert python to matlab

--- Verasonics_Matlab/Cv_conersion.m
+++ Verasonics_Matlab/Cv_conersion.m
@@ -2,15 +2,9 @@ clear all
 close all
 clc
 
-
-
-
-
 rosshutdown;
 
 rosinit
-
-
 
 PAIMG_sub = rossubscriber('PA_IMG', 'std_msgs/Float64MultiArray');
 k = 1; t1=0;
@@ -28,20 +22,3 @@ fprintf(['Frame #',num2str(k),'  F=',num2str(1/(t-t1)),'Hz\n'])
 k = k+1;
 t1 = t;
 end
-
-
-rstart = 140;
-rend =348;
-cstart = 195;
-cend = 235;
-
-ROI_image = ROI_creation(PAIMG,rstart,rend,cstart,cend)
-
-function  ROI_image = ROI_creation(image,row_start, row_end, col_start,col_end)
-    ROI_frame = image(row_start:row_end, col_start:col_end)
-
-    ROI_image = ROI_frame;
-    return ROI_image;
-
-end
-

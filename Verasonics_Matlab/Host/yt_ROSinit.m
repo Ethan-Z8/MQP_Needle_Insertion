c80d59d87b42008795097114a50ffd924be20607
Ethan Zhong <116708153+Ethan-Z8@users.noreply.github.com>
2025-05-01 19:16:50 -0400

    Update yt_ROSinit.m

--- Verasonics_Matlab/Host/yt_ROSinit.m
+++ Verasonics_Matlab/Host/yt_ROSinit.m
@@ -1,9 +1,10 @@
 global PA_IMG_pub PA_IMG_msg local_ip
 rosshutdown;
+URI = '';
+ip = 'xxx.xxx.x.xxx'
+ros_master_uri = URI; % need to check everytime
 
-ros_master_uri = 'http://192.168.0.197:11311'; % need to check everytime
-
-local_ip = '192.168.0.157';                    % need to check everytime
+local_ip = ip;                    % need to check everytime
 
 setenv('ROS_MASTER_URI', ros_master_uri);
 setenv('ROS_IP', local_ip);
